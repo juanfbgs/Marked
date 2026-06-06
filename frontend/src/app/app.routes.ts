9e05e6b9f1d6bfaf9eade930f2a77b6c7ab34035
juanfbgs <juanfrburgos@gmail.com>
2026-06-06 12:06:58 -0300

    feature(frontend): home page component created

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from '@features/home/home';
 
 export const routes: Routes = [
+    { path: '', component: Home },
     { path: 'auth', loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES) },
 ];
